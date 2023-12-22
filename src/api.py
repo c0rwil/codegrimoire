919ca1e2f3a7c06b96cb6e3aaa413601609cbb24
mikl-o <charliecwilliams9@gmail.com>
2023-12-22 15:34:00 -0700

    token by env

--- src/api.py
+++ src/api.py
@@ -5,11 +5,13 @@ from starlette.responses import RedirectResponse
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 from src.code_grimoire import CodeGrimoire  # Import your class
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
 CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
-
+TOKEN = os.getenv("TOKEN")
 app = FastAPI(title="CodeGrimoire")
 app.add_middleware(
     CORSMiddleware,
@@ -18,7 +20,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-def oauth_token(token: str = None):
+def oauth_token(token= TOKEN):
     if token is None:
         raise HTTPException(status_code=401, detail="Not authenticated")
     return token
@@ -43,7 +45,8 @@ async def callback(code: str):
     # Returning access token to the frontend (or client)
     return {"access_token": access_token}
 @app.post("/analyze")
-async def analyze_repos(token: str = Depends(oauth_token)):
+async def analyze_repos():
+    token: str = TOKEN
     grimoire = CodeGrimoire(token)
     grimoire.analyze_repos()
     return grimoire.total_lines, grimoire.repos_languages
